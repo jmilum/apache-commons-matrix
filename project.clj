d360ac3ceeaf82d94ac589b3f4fa5490576e6093
Jeremy Milum <jmilum@gmail.com>
2019-06-03 20:49:44 -0400

    updated deps and converted from lein to tools.deps

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject mthomure/apache-commons-matrix "0.3.1-SNAPSHOT"
-  :description "Implementation of core.matrix backed by Apache Commons Math matrices"
-  :url "https://github.com/Adereth/apache-commons-matrix"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [net.mikera/core.matrix "0.57.0"]
-                 [org.apache.commons/commons-math3 "3.6.1"]])
